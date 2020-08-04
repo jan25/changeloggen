1a9ce88858ffa8d0c41df05bf10009697e5abab5
Abhilash Gnan <abhilashgnan@gmail.com>
2020-08-04 23:24:54 +0200

    hacky local git repo parser

--- lib/git.ex
+++ lib/git.ex
@@ -2,7 +2,7 @@ defmodule Git do
   def parse_origin_url() do
     case System.cmd("git", String.split("remote get-url origin", " ")) do
       {url, _exit_code = 0} ->
-        {:ok, String.trim(url, "\n")}
+        {:ok, String.trim(url, ".git\n")}
 
       _ ->
         {:error, "Current directory not a git repository"}
